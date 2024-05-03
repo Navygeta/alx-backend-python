d724eb8256522b2db87942f9170becfd94b1b822
Navygeta <tinashenavonika@gmail.com>
2024-05-03 04:31:03 +0200

    all

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""adds two floats with type annotation"""
+
+
+def add(a: float, b: float) -> float:
+    """Adds two floats"""
+    return a + b
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Concats and return new string"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concats two strings"""
+    return str1 + str2
